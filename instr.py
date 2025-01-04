760f8a44fe22d7a1ea945feff1b5a20e53167268
twentiecker <twentiecker@gmail.com>
2025-01-04 13:39:06 +0700

    Add text details

--- instr.py
+++ instr.py
@@ -4,14 +4,21 @@ win_width, win_height = 1000, 600
 
 txt_hello = 'Welcome to the health status ...'
 txt_next = 'Start'
-txt_instruction = 'This application allows you to use the Ruffier test ...' 
+txt_instruction = ('This application allows you to use the Rufier test to make an initial diagnosis of your health.\n'
+                   'The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.\n'
+                   'The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds; \n'
+                   'then, within 45 seconds, the subject performs 30 squats.\n'
+                   'When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds\n'
+                   'and then for the last 15 seconds of the first minute of the recovery period.\n'
+                   'Important! If you feel unwell during the test (dizziness,\n'
+                   'tinnitus, shortness of breath, etc.), stop the test and consult a physician.' ) 
 txt_title = 'Kesehatan'
 txt_name = 'Enter your full name'
 txt_hintname = "Full name"
 txt_hintage = "0"
-txt_test1 = 'lie on your back and take ...'
-txt_test2 = 'Perform 30 squads in 45 seconds ...'
-txt_test3 = 'Lie on your back and take your pulse for the first ...'
+txt_test1 = 'Lie on your back and take your pulse for 15 seconds. Click the "Start first test" button to start the timer.\nWrite down the result in the appropriate field.'
+txt_test2 = 'Perform 30 squats in 45 seconds. To do this, click the "Start doing squats" button\nto start the squat counter.'
+txt_test3 = 'Lie on your back and take your pulse for the first 15 seconds of the minute, then for the last 15 seconds of the minute.\nPress the "Start final test" button to start the timer.\nThe seconds that should be measured are indicated in green and the minutes that should not be measured are indicated in black. Write down the results in the appropriate fields.'
 txt_sendresults = 'Send the result'
 txt_hinttest1 = '0'
 txt_hinttest2 = '0'
